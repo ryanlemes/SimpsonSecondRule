9585934b9dc58d516588b7ea15fd54ad0abb59c5
Ryan Lemes <33068599+ryanlemes@users.noreply.github.com>
2017-11-14 22:12:06 -0200

    First Code

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/// Integration method: Simpson Second Rule
+/// Developed by:
+///     Gustavo Loschi
+///     Ryan Lemes
+/// For Numeral calculus discipline
+
+
+
+
+int main()
+{
+    float h, x, SomaMultiplo3, SomanaoMultiplo3, R;
+    int i, n;
+
+
+    if(n == 0)
+        printf("Divisão por Zero!");
+    else if(n < 0)
+        printf("Intervalo Inválido");
+    else
+    {
+        if(n % 3 != 0)
+            n = 3 * (n / 3) + 1)
+        h = ()
+    }
+
+
+    return 0;
+}
